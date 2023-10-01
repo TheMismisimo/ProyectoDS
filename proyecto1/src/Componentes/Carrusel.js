a832496b609d9aca53661ea150033fcfb341a710
TheMismisimo <armontecinosg@gmail.com>
2023-10-01 19:08:07 -0300

    carrusel funcionando

--- proyecto1/src/Componentes/Carrusel.js
+++ proyecto1/src/Componentes/Carrusel.js
@@ -1,49 +1,36 @@
-import React, { useState } from "react";
-import { Carousel, Button } from "react-bootstrap";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
+import { Carousel } from 'react-bootstrap';
+import '../Estilos/Carrusel.css'
 
-function Carrusel() {
-    const [imagenes, setImagenes] = useState([
-        {
-          src: "https://github.com/TheMismisimo/ProyectoDS/blob/81b55415c954a56cf8f10719af49eed3f41880fb/proyecto1/src/Imagenes/martillo.jpg",
-          alt: "Imagen 1",
-        },
-        {
-          src: "https://github.com/TheMismisimo/ProyectoDS/blob/81b55415c954a56cf8f10719af49eed3f41880fb/proyecto1/src/Imagenes/mas1.jpg",
-          alt: "Imagen 2",
-        },
-        {
-          src: "https://github.com/TheMismisimo/ProyectoDS/blob/81b55415c954a56cf8f10719af49eed3f41880fb/proyecto1/src/Imagenes/myd.jpg",
-          alt: "Imagen 3",
-        },
-      ]);
-
-    const siguienteImagen = () => {
-    const indiceActual = imagenes.findIndex((imagen) => imagen.src === document.querySelector(".carousel-item.active img").src);
-    const indiceSiguiente = (indiceActual + 1) % imagenes.length;
-
-    setImagenes([...imagenes, imagenes.splice(indiceActual, 1)[0]]);
-    };
-
-    const anteriorImagen = () => {
-        const indiceActual = imagenes.findIndex((imagen) => imagen.src === document.querySelector(".carousel-item.active img").src);
-        const indiceAnterior = (indiceActual - 1 + imagenes.length) % imagenes.length;
-
-        setImagenes([...imagenes, imagenes.splice(indiceAnterior, 1)[0]]);
-    };
-    
-    return (
-        <div className="container">
-          <Carousel>
-            {imagenes.map((imagen, indice) => (
-              <Carousel.Item key={indice}>
-                <img src={imagen.src} alt={imagen.alt} />
-              </Carousel.Item>
-            ))}
-          </Carousel>
-          <Button onClick={anteriorImagen}>Anterior</Button>
-          <Button onClick={siguienteImagen}>Siguiente</Button>
-        </div>
-      );
+const Carrusel = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-25 mx-auto"
+          src="https://i.pinimg.com/564x/80/9b/25/809b2577c94c73588f2eebdd1804eae2.jpg"
+          alt="Imagen 1"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-25 mx-auto"
+          src="https://i.pinimg.com/564x/f4/17/16/f41716d65acb3d517fa427b74c3a8e4d.jpg"
+          alt="Imagen 2"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-25 mx-auto"
+          src="https://i.pinimg.com/564x/27/34/1d/27341d18902597797baf196d345ef830.jpg"
+          alt="Imagen 3"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
 }
+  
+  
 
 export default Carrusel
