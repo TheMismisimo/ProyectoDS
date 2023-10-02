3110eeb35b04ca8dce01042cdd781fc313518c56
TheMismisimo <armontecinosg@gmail.com>
2023-10-02 12:23:24 -0300

    weas x eliminadas

--- proyecto1/src/App.js
+++ proyecto1/src/App.js
@@ -2,18 +2,10 @@ import Carrusel from "./Componentes/Carrusel";
 import BarraNav from "./Componentes/BarraNav";
 
 
-function retornarX(){
-  return Math.trunc(Math.random()*10);
-}
 
 
 function App() 
 {
-  const siglo = 21
-  const persona = {
-    nombre: 'pepe',
-    edad: 24
-  }
 
   return (
     <div className="App">
