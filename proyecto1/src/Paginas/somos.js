392442b6de3ef4e836b525cf9bf9fa5d8b9293b9
Th3Extr3mist <146364367+Th3Extr3mist@users.noreply.github.com>
2023-10-01 23:13:21 -0300

    puse el somos

--- proyecto1/src/Paginas/somos.js
+++ proyecto1/src/Paginas/somos.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function somos() {
   return (
-    <div>somos</div>
+    <div>
+      <h1>¿Quienes somos?</h1>
+      <p>Somos una empresa dedicada a servicios integrales, de logística, ingeniería y comercialización, nos dedicamos a apoyar a nuestros clientes, dándoles soluciones innovadoras, oportunas y que agregan valor a sus propias actividades, somos su mayor aliado y el apoyo que necesitan.</p>
+    </div>
   )
 }
 
