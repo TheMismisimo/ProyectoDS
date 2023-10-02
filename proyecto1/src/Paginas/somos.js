af07bace4d1a68882e6958061af1ae767bd90108
TheMismisimo <armontecinosg@gmail.com>
2023-10-01 23:53:41 -0300

    responsive

--- proyecto1/src/Paginas/somos.js
+++ proyecto1/src/Paginas/somos.js
@@ -4,7 +4,7 @@ function somos() {
   return (
     <div>
       <h1 className='mb-5'>¿Quienes somos?</h1>
-      <p>Somos una empresa dedicada a servicios integrales, de logística, ingeniería y comercialización, nos dedicamos a apoyar a nuestros clientes, dándoles soluciones innovadoras, oportunas y que agregan valor a sus propias actividades, somos su mayor aliado y el apoyo que necesitan.</p>
+      <p className='justify-content-between'>Somos una empresa dedicada a servicios integrales, de logística, ingeniería y comercialización, nos dedicamos a apoyar a nuestros clientes, dándoles soluciones innovadoras, oportunas y que agregan valor a sus propias actividades, somos su mayor aliado y el apoyo que necesitan.</p>
     </div>
   )
 }
