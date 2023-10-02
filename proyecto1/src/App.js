ef91215353c45c4ce461789ea42307f526b4a29c
Th3Extr3mist <146364367+Th3Extr3mist@users.noreply.github.com>
2023-10-02 17:57:17 -0300

    baba

--- proyecto1/src/App.js
+++ proyecto1/src/App.js
@@ -6,31 +6,27 @@ import{useState, useEffect} from 'react';
 
 function App() 
 {
+  const [articulos, setArticulos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.example.com/data')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setArticulos(data);
+      });
+  }, []);
+  
 
   return (
     <div className="App">
-      <BarraNav/>
-      <p className="mb-5"></p>
-      <Carrusel/>
-      <p>Esta es la pagina de inicio de la empresa God tier sa</p>
+      <BarraNav />
+      <p className="mb-5">Nombre del personaje: {articulos.name || 'Cargando...'}</p>
+      <Carrusel />
+      <p>Esta es la página de inicio de la empresa God tier sa</p>
     </div>
   );
-}
-
+  
+}  
 export default App;
-
-
-
-
-
-  const[data, setData]= useState([])
-
-  useEffect(() =>{
-    fetch('https://scratchya.com.ar/react/datos.php')
-    .then((Response)=>{
-      return Response.json()
-    })
-    .then((articulos)=>{
-      setArticulos(articulos)
-    })
-  },[])
