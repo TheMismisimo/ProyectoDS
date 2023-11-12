bd67254a775a7d15214197f5a24946e38406c45a
TheMismisimo <armontecinosg@gmail.com>
2023-11-12 20:56:15 -0300

    justificacion añadida + margen a los textos

--- proyecto1/src/Paginas/Nosotros.js
+++ proyecto1/src/Paginas/Nosotros.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import BarraNav from "../Componentes/BarraNav";
 import "../Estilos/Fondo.css";
+import '../Estilos/Justificado.css'
+
 
 
 function Nosotros() {
@@ -10,18 +12,18 @@ function Nosotros() {
       <div className='mb-5'>
         <h1>Sobre Nosotros</h1>
       </div>
-      <div >
-        <p className='justify-content-between'>Ser la empresa líder en el desarrollo y aplicación de soluciones integrales de ingeniería, logística y comercialización a nuestros clientes para contribuir a su competitividad y al progreso social.</p>
-        <p className='justify-content-between'>Equipo:</p>
-        <p className='justify-content-between'>Gerencia General: Ricardo Meneses</p>
+      <div>
+        <p className='texto-justificado'>Ser la empresa líder en el desarrollo y aplicación de soluciones integrales de ingeniería, logística y comercialización a nuestros clientes para contribuir a su competitividad y al progreso social.</p>
+        <p className='texto-justificado'>Equipo:</p>
+        <p className='texto-justificado'>Gerencia General: Ricardo Meneses</p>
 
-        <p className='justify-content-between'>Gerencia de Operaciones: Rafael Urbina</p>
+        <p className='texto-justificado'>Gerencia de Operaciones: Rafael Urbina</p>
 
-        <p className='justify-content-between'>Gerencia de Personas: Jéssica Araya</p>
+        <p className='texto-justificado'>Gerencia de Personas: Jéssica Araya</p>
 
-        <p className='justify-content-between'>Gerencia de Ingeniería: María Polanco</p>
+        <p className='texto-justificado'>Gerencia de Ingeniería: María Polanco</p>
 
-        <p className='justify-content-between'>Gerencia de Seguridad: Bowie Bernardo</p>
+        <p className='texto-justificado'>Gerencia de Seguridad: Bowie Bernardo</p>
       </div>
       <div className='mb-5'>
         <img src='image001.jpg' alt='organigrama' className='d-block w-25 mx-auto'></img>
