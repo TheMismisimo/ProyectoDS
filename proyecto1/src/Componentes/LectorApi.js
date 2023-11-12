a4ec821d719b84ce31343bbd47c512b9e08124e9
TheMismisimo <armontecinosg@gmail.com>
2023-11-12 19:27:54 -0300

    Implementacion api unsplash

--- proyecto1/src/Componentes/LectorApi.js
+++ proyecto1/src/Componentes/LectorApi.js
@@ -1,50 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// en teooooriaaaaa funciona, but again it's just a theory
+function LectorApi() {
 
-function NameForm() {
-  const [names, setNames] = useState([]);
-  const [selectedName, setSelectedName] = useState('');
-  const [additionalInfo, setAdditionalInfo] = useState('');
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
-    // Make API call to fetch names
-    axios.get('your-api-endpoint')
-      .then(response => {
-        setNames(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching names:', error);
-      });
+    const imageIds = ['BNBA1h-NgdY', 'Hcfwew744z4', 'VBLHICVh-lI', 'e6n7uoEnYbA']; // Reemplaza con las ID reales de las imágenes que deseas buscar
+    const apiKey = '8F4i9cjH6Yd7fYGMeVRK2Lr3_04AXf2BuGkC6fNTFRM';
+  
+    const fetchImageDetails = async (imageId) => {
+      try {
+        const response = await axios.get(`https://api.unsplash.com/photos/${imageId}`, {
+          params: {
+            client_id: apiKey,
+          },
+        });
+
+        return response.data;
+      } catch (error) {
+        console.error(`Error al obtener detalles de la imagen con ID ${imageId}:`, error);
+        return null;
+      }
+    };
+
+    const fetchImages = async () => {
+      try {
+        const imagePromises = imageIds.map(async (imageId) => {
+          return await fetchImageDetails(imageId);
+        });
+
+        const imageDetails = await Promise.all(imagePromises);
+        setImages(imageDetails.filter(image => image !== null));
+      } catch (error) {
+        console.error('Error al obtener imágenes de Unsplash:', error);
+      }
+    };
+
+    fetchImages();
   }, []);
-  const handleSubmit = (e) => {
-    e.preventDefault();
 
-    // Find the selected name
-    const selectedNameObject = names.find((name) => name.id === selectedName);
-
-    if (selectedNameObject) {
-      // Display more information
-      setAdditionalInfo(`ID: ${selectedNameObject.id}, Additional Info: ${selectedNameObject.additionalInfo}`);
-    }
-  };
   return (
     <div>
-      <label>Select a name:</label>
-      <select value={selectedName} onChange={(e) => setSelectedName(e.target.value)}>
-        <option value="">Select a name</option>
-        {names.map((name) => (
-          <option key={name.id} value={name.id}>
-            {name.name}
-          </option>
+      <div className='row'>
+        {images.map((image, index) => (
+          <div className={`col-lg-4 col-md-12 mb-4 mb-lg-0 ${index % 2 === 0 ? 'mx-4' : ''}`}>
+            <img
+            key={image.id}
+            src={image.urls.small}
+            alt={image.alt_description}
+            className="w-100 shadow-1-strong rounded"
+            onClick={() => window.open(image.links.html)} // Abre la imagen en una nueva pestaña al hacer clic
+            />
+          </div>
         ))}
-      </select>
-
-      <button onClick={handleSubmit}>Submit</button>
-
-      {/* Display additional information */}
-      {additionalInfo && <p>{additionalInfo}</p>}
+      </div>
     </div>
   );
-}
+};
+export default LectorApi;
--- proyecto1/src/Paginas/Galeria.js
+++ proyecto1/src/Paginas/Galeria.js
@@ -2,7 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
 import BarraNav from "../Componentes/BarraNav";
 import "../Estilos/Fondo.css";
-
+import LectorApi from '../Componentes/LectorApi';
 
 
 function Galeria() {
@@ -10,44 +10,8 @@ function Galeria() {
     <div>
       <BarraNav/>
       <h1>Nuestra galeria</h1>
-      <div class="row">
-        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-          <div class="mb-4 mx-4">
-            <img
-              src="https://cdn.pixabay.com/photo/2018/03/10/12/00/teamwork-3213924_1280.jpg"
-              class="w-100 shadow-1-strong rounded "
-              alt="Choque de puños"
-            />
-          </div>
-
-          <div class="mb-4 mx-4">
-            <img
-              src="https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501_1280.jpg"
-              class="w-100 shadow-1-strong rounded"
-              alt="Reunion"
-            />
-          </div>
-        </div>
-
-        <div class="col-lg-4 mb-4 mb-lg-0">
-          <div class="mb-4 mx-4">
-            <img
-              src="https://cdn.pixabay.com/photo/2015/01/08/18/26/man-593333_1280.jpg"
-              class="w-100 shadow-1-strong rounded"
-              alt="Toma de datos"
-            />
-          </div>
-
-          <div class="mb-4 mx-4">
-            <img
-              src="https://cdn.pixabay.com/photo/2015/01/09/11/08/startup-594090_1280.jpg"
-              class="w-100 shadow-1-strong rounded"
-              alt="Apuntes grupales"
-            />
-          </div>
-        </div>
-      </div>
-
+      
+      <LectorApi/>
     </div>
     
   )
