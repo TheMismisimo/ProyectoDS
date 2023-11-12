d3ebbe944bd815e45d92263331c6d16f4cac803c
ShioHernandez <rhern0809@gmail.com>
2023-11-12 18:52:49 -0300

    implementacion Cartitas

--- proyecto1/src/Componentes/Cartitas.js
+++ proyecto1/src/Componentes/Cartitas.js
@@ -4,9 +4,62 @@ import React from 'react'
 function Cartitas(){
     return(
         <div>
-            <h1>Cartitas</h1>
+      <h1>Cartitas</h1>
+
+      <div className="wrapper">
+        <div className="cols">
+          <div className="col" onTouchStart={() => this.classList.toggle('hover')}>
+            <div className="container">
+              <div className="front" style={{ backgroundImage: 'url(https://unsplash.it/500/500/)' }}>
+                <div className="inner">
+                  <p>Logística Integral</p>
+                  <span>Soluciones logísticas</span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <p>soluciones logísticas integrales para empresas en busca de un socio confiable en el manejo de su cadena de suministro.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col" onTouchStart={() => this.classList.toggle('hover')}>
+            <div className="container">
+              <div className="front" style={{ backgroundImage: 'url(https://unsplash.it/500/500/)' }}>
+                <div className="inner">
+                  <p>Comercialización</p>
+                  <span>Servicios logísticas</span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <p> tu fuente confiable para productos al por mayor y al detalle.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div className="col" onTouchStart={() => this.classList.toggle('hover')}>
+            <div className="container">
+              <div className="front" style={{ backgroundImage: 'url(https://unsplash.it/500/500/)' }}>
+                <div className="inner">
+                  <p>Comercialización</p>
+                  <span>Servicios logísticas</span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <p> tu fuente confiable para productos al por mayor y al detalle.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          {/* Add more card components as needed */}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Cartitas;
