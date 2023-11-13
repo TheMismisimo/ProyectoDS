113751cd07ca33ff02ab7989ad1e27e89a9f50a6
TheMismisimo <armontecinosg@gmail.com>
2023-11-12 22:35:13 -0300

    modificacion cartas

--- proyecto1/src/Componentes/Cartitas.js
+++ proyecto1/src/Componentes/Cartitas.js
@@ -1,79 +1,63 @@
+import React, { useState } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../Estilos/Cartitas.css";
-import React from "react";
+
+// ... (previous imports and code)
 
 function Cartitas() {
-  return (
-    <div>
-      <h1>Cartitas</h1>
-
-      <div className="wrapper">
-        <div className="cols">
-          <div className="col" onTouchStart={() => this.classList.toggle("hover")}>
-            <div className="container">
-              <div
-                className="front"
-                style={{ backgroundImage: "url(https://unsplash.it/500/500/)" }}
-              >
-                <div className="inner">
-                  <p>Logística Integral</p>
-                  <span>Soluciones logísticas</span>
-                </div>
-              </div>
-              <div className="back">
-                <div className="inner">
-                  <p>
-                    Soluciones logísticas integrales para empresas en busca de un
-                    socio confiable en el manejo de su cadena de suministro.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col" onTouchStart={() => this.classList.toggle("hover")}>
-            <div className="container">
-              <div
-                className="front"
-                style={{ backgroundImage: "url(https://unsplash.it/500/500/)" }}
-              >
-                <div className="inner">
-                  <p>Comercialización</p>
-                  <span>Servicios logísticas</span>
-                </div>
-              </div>
-              <div className="back">
-                <div className="inner">
-                  <p>
-                    Tu fuente confiable para productos al por mayor y al detalle.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="col" onTouchStart={() => this.classList.toggle("hover")}>
-            <div className="container">
-              <div
-                className="front"
-                style={{ backgroundImage: "url(https://unsplash.it/500/500/)" }}
-              >
-                <div className="inner">
-                  <p>Comercialización</p>
-                  <span>Servicios logísticas</span>
-                </div>
-              </div>
-              <div className="back">
-                <div className="inner">
-                  <p>
-                    Tu fuente confiable para productos al por mayor y al detalle.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default Cartitas;
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+  
+    const handleMouseEnter = (index) => {
+      setHoveredIndex(index);
+    };
+  
+    const handleMouseLeave = () => {
+      setHoveredIndex(null);
+    };
+  
+    const cardsData = [
+      {
+        title: "Logística Integral",
+        subtitle: "Soluciones logísticas",
+        description:
+          "Soluciones logísticas integrales para empresas en busca de un socio confiable en el manejo de su cadena de suministro.",
+      },
+      {
+        title: "Comercialización",
+        subtitle: "Servicios logísticas",
+        description:
+          "Tu fuente confiable para productos al por mayor y al detalle.",
+      },
+    ];
+  
+    return (
+      <Container>
+        <h1 className="text-center mt-4 mb-5">Cartitas</h1>
+        <Row className="justify-content-center">
+          {cardsData.map((card, index) => (
+            <Col
+              key={index}
+              md={4}
+              sm={6}
+              xs={12}
+              className="mb-4"
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+            >
+              <Card className={`hover-effect ${hoveredIndex === index ? "hovered" : ""}`}>
+                <Card.Body>
+                  <Card.Title>{card.title}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">{card.subtitle}</Card.Subtitle>
+                  <Card.Text>{card.description}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
+  
+  export default Cartitas;
+  
